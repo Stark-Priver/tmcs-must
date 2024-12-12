ac8ee486d47af065c11411862a673183a4beea00
Priver <priverdeprince@yahoo.com>
2024-12-12 14:30:23 +0300

    Added the Allowed Hosts

--- tmcsmust/settings.py
+++ tmcsmust/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p%svrndl8tie(vshq!$s))93d5*unw#4z+bzx2cc+mmr!liwyx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = ['https://eager-shape-40883.pktriot.net',]
 
